f6764a8985033b39569539f099d804d49593c86a
nikhil pawar <nspawar153@gmail.com>
2022-06-06 19:27:43 +0530

    copmleted upto card component and installed react-icons

---
+++ src/FeedbackData.js
@@ -0,0 +1,19 @@
+const feedbackData = [
+    {
+        id: 1,
+        rating: 8,
+        text: "This is text 1"
+    },
+    {
+        id: 2,
+        rating: 10,
+        text: "This is text 2"
+    },
+    {
+        id: 3,
+        rating: 7,
+        text: "This is text 3"
+    }
+];
+
+export default feedbackData;
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,17 @@
+import Card from './shared/Card'
+
+function FeedbackItem({item}) {
+    
+    return (
+        <Card>
+            <div className="num-display">{ item.rating }</div>
+            <div className="text-display">{ item.text }</div>
+        </Card>
+    )
+}
+
+Card.defaultProps = {
+    reverse: false,
+}
+
+export default FeedbackItem
